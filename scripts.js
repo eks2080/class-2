41ecdcd2a94b8b5c7f9fb471bf67be632f6a3af0
eks2080 <eks2080@nyu.edu>
2024-03-28 19:34:12 -0400

    port jffiddle into html, css, and js files

---
+++ scripts.js
@@ -0,0 +1,16 @@
+$('.button-1').on('click', function() {
+    $('#openingyear').html('1964');
+    })
+  
+  $('.button-2').on('click', function() {
+    $('#openingyear').html('1947');
+    })
+  
+  $('.button-3').on('click', function() {
+    $('#openingyear').html('1904');
+    })
+  
+  $('.button-4').on('click', function() {
+    $('#openingyear').html('1976');
+    })
+  
